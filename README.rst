d43421a31316ab1943a234f175179343d93b8e96
sgalkina <galkina.s.a@gmail.com>
2017-03-25 22:30:12 +0100

    homework 1

---
+++ README.rst
@@ -0,0 +1,3 @@
+# haskell_practice
+
+Having fun with http://www.cis.upenn.edu/~cis194/spring13/lectures.html
--- tasks/hw1.hs
+++ tasks/hw1.hs
@@ -1,5 +1,38 @@
--- Convert positive Integers to a list of digits
+-- Convert positive Integer to a list of digits
 toDigits :: Integer -> [Integer]
-toDigits x = [x, x]
+toDigits x
+  | x <= 0 = []
+  | otherwise = toDigits (div x 10) ++ [mod x 10]
 
-main = print (toDigits 1234)
+-- Convert positive Integer to a reversed list of digits
+toDigitsRev :: Integer -> [Integer]
+toDigitsRev x
+  | x <= 0 = []
+  | otherwise = (mod x 10) : toDigitsRev (div x 10)
+
+-- Double every other number
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther [] = []
+doubleEveryOther [x] = [x]
+doubleEveryOther (x:y:zs) = x : y * 2 : doubleEveryOther zs
+
+--Calculate the sum of all digits
+sumDigits :: [Integer] -> Integer
+sumDigits [] = 0
+sumDigits (x:xs) = sum (toDigits x) + sumDigits xs
+
+--Indicates whether an Integer could be a valid credit card number
+validate :: Integer -> Bool
+validate x = (sumDigits (doubleEveryOther (toDigitsRev x))) `mod` 10 == 0
+
+type Peg = String
+type Move = (Peg, Peg)
+--Given the number of discs and names for the three pegs, hanoi
+--should return a list of moves to be performed to move the stack of
+--discs from the first peg to the second
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi 0 a b c = []
+hanoi 1 a b c = [(a, b)]
+hanoi x a b c = hanoi (x - 1) a c b ++ hanoi 1 a b c ++ hanoi (x - 1) c b a
+
+main = print (hanoi 3 "a" "b" "c")
